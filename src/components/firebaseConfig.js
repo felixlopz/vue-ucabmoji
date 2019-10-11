cd4b0207d05f053a29d0e346b2359cdb1ba56098
felix <felixcodesg@gmail.com>
2019-10-11 07:21:52 -0400

    removing api key

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,11 +1,11 @@
 // enter to firebase dashboard to get this config
 export default {
-  apiKey: "AIzaSyDaNFIny3JMgW5Ftca3awcjprTIwBY5ybI",
-  authDomain: "proyecto-ucabmoji.firebaseapp.com",
-  databaseURL: "https://proyecto-ucabmoji.firebaseio.com",
-  projectId: "proyecto-ucabmoji",
-  storageBucket: "proyecto-ucabmoji.appspot.com",
-  messagingSenderId: "248422988570",
-  appId: "1:248422988570:web:b5297045e3e5a897"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
   
