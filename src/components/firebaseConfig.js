99f81f419e97a367b98ec29744bcf8e3219cf442
felixlopz <felixlopzd@gmail.com>
2020-02-20 09:51:32 -0400

    adding keys

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,11 +1,20 @@
 // enter to firebase dashboard to get this config
+// export default {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   databaseURL: process.env.DATABASE_URL,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID
+// };
+//   
 export default {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
-};
-  
+  apiKey: "AIzaSyDaNFIny3JMgW5Ftca3awcjprTIwBY5ybI",
+  authDomain: "proyecto-ucabmoji.firebaseapp.com",
+  databaseURL: "https://proyecto-ucabmoji.firebaseio.com",
+  projectId: "proyecto-ucabmoji",
+  storageBucket: "proyecto-ucabmoji.appspot.com",
+  messagingSenderId: "248422988570",
+  appId: "1:248422988570:web:b5297045e3e5a897"
+};  
